c2be5e96d3765855f08c1ffba78a42cdf8e7d176
aptopspeed <maplestory.ssso@gmail.com>
2024-09-24 08:01:44 +0700

    add color and text

--- src/ShareButton.js
+++ src/ShareButton.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const SharedButton = ({ text, onClick }) => (
+  <>
+  <h1 style={{color: "red"}}>{text}</h1>
   <button onClick={onClick}>{text}</button>
+  </>
 );
 
 export default SharedButton;
